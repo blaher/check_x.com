b83453f203d20a8b7f4201603303074293a18aca
ben <ben@performanceadmin.com>
2017-07-14 13:24:07 -0400

    Sending an email to me if x.com changes

---
+++ check.php
@@ -0,0 +1,17 @@
+<?php
+$email_address = "user@domain.com";
+
+echo "Starting script...\n";
+
+while (file_get_contents("http://x.com") === "x") {
+  echo ".";
+
+  sleep(60);
+}
+echo "\nx.com has changed!\n";
+
+echo "Alerting user...\n";
+mail($email_address, "x.com has changed!", "Please check the website now.");
+
+echo "Killing script...\n";
+exit();
